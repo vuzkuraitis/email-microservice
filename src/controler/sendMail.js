14282b45ed5cf74469fd1aee342e20c1a90e8735
Vidmantas Uzkuraitis <v.uzkuraitis@yahoo.com>
2022-07-30 17:02:29 +0300

    email fix

--- src/controler/sendMail.js
+++ src/controler/sendMail.js
@@ -1,8 +1,10 @@
 import config from "../config.js";
 import nodemailer from "nodemailer";
+import path from "path";
 
 const sendMail = async (req, res) => {
   const { to, subject, html } = req.body;
+  const __dirname = path.resolve();
   const transporter = nodemailer.createTransport(config.mailConfig);
 
   const info = await transporter.sendMail({
@@ -10,6 +12,13 @@ const sendMail = async (req, res) => {
     to,
     subject,
     html,
+    attachments: [
+      {
+        filename: "logo.jpeg",
+        path: `${__dirname}/src/logo.jpeg`,
+        cid: "logo",
+      },
+    ],
   });
 
   return res.send({ msg: `Email sent`, id: info.messageId });
